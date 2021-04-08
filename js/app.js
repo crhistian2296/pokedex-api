a5baf9e2b5fe7c0f36ad83a83e9a4d8cf5e552aa
Crhistian <matrixcristian21@gmail.com>
2021-04-07 23:34:46 -0500

    Done

--- js/app.js
+++ js/app.js
@@ -1,36 +1,55 @@
 'use strict'
 
-document.addEventListener('DOMContentLoaded', () => {
-    const random = getRandomInt(1, 150);
-    fetchData(random);
-})
-
-//returns a integer wich values is between max and min parameters
-const getRandomInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-//prints card
-const printCard = (pokemon) => {
-    const flex = document.querySelectorAll('.flex');
-    const template = document.querySelector('#template-card').content;
-    const clone = template.cloneNode(true);
-    const fragment = document.createDocumentFragment();
+document.addEventListener("DOMContentLoaded", () => {
+    //151 pokemons of the first gen
+    const ramdom = getRandomInt(1, 152);
+    fetchData(ramdom);
+});
 
-    clone.querySelector('.card-body-img').setAttribute('src', pokemon.sprites.other.dream_world.front_default);
-
-    fragment.appendChild(clone);
-    flex.appendChild(fragment);
-}
-
-//get petition => pokemon.json
 const fetchData = async (id) => {
     try {
         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
         const data = await res.json();
-        console.log(data);
-        printCard(data)
+        console.log(`Pokemon No.${id}`);
+
+        //pokemon object (faster and easier to understand)
+        const pokemon = {
+            img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`,
+            imgJuego: data.sprites.front_default,
+            imgCvg: data.sprites.other.dream_world.front_default,
+            nombre: data.name,
+            experiencia: data.base_experience,
+            hp: data.stats[0].base_stat,
+            ataque: data.stats[1].base_stat,
+            defensa: data.stats[2].base_stat,
+            especial: data.stats[3].base_stat,
+        };
+
+        console.log(pokemon);
+        printCard(pokemon);
     } catch (error) {
         console.log(error);
     }
-}
+};
+
+const printCard = (pokemon) => {
+    const flex = document.querySelector(".flex");
+    const template = document.getElementById("card").content;
+    const clone = template.cloneNode(true);
+    const fragment = document.createDocumentFragment();
+
+    clone.querySelector(".card-body-img").setAttribute("src", pokemon.imgCvg);
+    // clone.querySelector('.card-body-img').setAttribute('src', pokemon.imgJuego)
+    clone.querySelector(".card-body-title").innerHTML = `${pokemon.nombre} <span>${pokemon.hp}hp</span>`;
+    clone.querySelector(".card-body-text").textContent = pokemon.experiencia + " exp";
+    clone.querySelectorAll(".card-footer-social h3")[0].textContent = pokemon.ataque + "K";
+    clone.querySelectorAll(".card-footer-social h3")[1].textContent = pokemon.especial + "K";
+    clone.querySelectorAll(".card-footer-social h3")[2].textContent = pokemon.defensa + "K";
+
+    fragment.appendChild(clone);
+    flex.appendChild(fragment);
+};
